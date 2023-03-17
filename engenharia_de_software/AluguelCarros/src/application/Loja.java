4134119554d959739e25ade8f9bca3e4304eb665
Alessandra Moreira <107525483+Alemoreira-00@users.noreply.github.com>
2023-03-17 19:03:40 -0300

    Add files via upload

---
+++ engenharia_de_software/AluguelCarros/src/application/Loja.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Loja {
+	private String nome;
+	private String local;
+	private List<Funcionario> funcionarios = new LinkedList<Funcionario>();
+	private List<Carro> carros = new LinkedList<Carro>();
+	
+
+}
